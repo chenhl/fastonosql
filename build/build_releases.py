712598678eebd6e3a1f70576bd51cbc25aca7647
topilski <atopilski@gmail.com>
2018-03-26 06:09:21 +0300

    Sign sources

--- build/build_releases.py
+++ build/build_releases.py
@@ -12,11 +12,10 @@ from pybuild_utils.base import utils
 def print_usage():
     print("Usage:\n"
           "[required] argv[1] cmake_root_path\n"
-          "[required] argv[2] expired utc timestamp\n"
-          "[optional] argv[3] platform\n"
-          "[optional] argv[4] architecture\n"
-          "[optional] argv[5] build system(\"ninja\", \"make\")\n"
-          "[optional] argv[6] packages for example(\"DEB RPM\")\n")
+          "[optional] argv[2] platform\n"
+          "[optional] argv[3] architecture\n"
+          "[optional] argv[4] build system(\"ninja\", \"make\")\n"
+          "[optional] argv[5] packages for example(\"DEB RPM\")\n")
 
 
 def print_message(progress, message):
@@ -34,35 +33,23 @@ if __name__ == "__main__":
         sys.exit(1)
 
     if argc > 2:
-        expired_time_str = sys.argv[2]
-        expired_time = datetime.utcfromtimestamp(int(expired_time_str))
-        cur_utc = datetime.utcnow()
-        if expired_time < cur_utc:
-            print("Expired utc time({0}) should be greater than current utc time({1})".format(expired_time, cur_utc))
-            print_usage()
-            sys.exit(1)
-    else:
-        print_usage()
-        sys.exit(1)
-
-    if argc > 3:
-        platform_str = sys.argv[3]
+        platform_str = sys.argv[2]
     else:
         platform_str = system_info.get_os()
 
-    if argc > 4:
-        arch_bit_str = sys.argv[4]
+    if argc > 3:
+        arch_bit_str = sys.argv[3]
     else:
         arch_bit_str = system_info.get_arch_name()
 
-    if argc > 5:
-        bs_str = sys.argv[5]
+    if argc > 4:
+        bs_str = sys.argv[4]
         bs = system_info.get_supported_build_system_by_name(bs_str)
     else:
         bs = None
 
-    if argc > 6:
-        packages = sys.argv[6].split()
+    if argc > 5:
+        packages = sys.argv[5].split()
     else:
         packages = []
 
@@ -74,9 +61,7 @@ if __name__ == "__main__":
     fastonosql_branding_options = utils.read_file_line_by_line_to_list(fastonosql_branding_file)
     # request.build(cmake_root, fastonosql_branding_options, 'build_fastonosql_' + platform_str, bs, packages, saver)
 
-    fastonosql_branding_options_trial = fastonosql_branding_options
-    fastonosql_branding_options_trial.append('-DEXPIRE_APPLICATION_UTC_TIME={0}'.format(expired_time_str))
-    request.build(cmake_root, fastonosql_branding_options_trial, 'build_fastonosql_' + platform_str + '_trial', bs,
+    request.build(cmake_root, fastonosql_branding_options, 'build_fastonosql_' + platform_str, bs,
                   packages, saver)
 
     # fastoredis build
@@ -84,7 +69,5 @@ if __name__ == "__main__":
     fastoredis_branding_options = utils.read_file_line_by_line_to_list(fastoredis_branding_file)
     # request.build(cmake_root, fastoredis_branding_options, 'build_fastoredis_' + platform_str, bs, packages, saver)
 
-    fastoredis_branding_options_trial = fastoredis_branding_options
-    fastoredis_branding_options_trial.append('-DEXPIRE_APPLICATION_UTC_TIME={0}'.format(expired_time_str))
-    request.build(cmake_root, fastoredis_branding_options_trial, 'build_fastoredis_' + platform_str + '_trial', bs,
+    request.build(cmake_root, fastoredis_branding_options, 'build_fastoredis_' + platform_str, bs,
                   packages, saver)
